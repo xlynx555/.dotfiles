80f8b97cba057b57ce163903596d3d7da57ded63
Den Leschenko <den.leschenko@gmail.com>
2026-03-22 13:20:19 +0100

    merge configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.local/share/gh/extensions/gh-fish/gh-copilot-alias.fish
     starship init fish | source
+    zoxide init fish | source
 end
 bind alt-backspace backward-kill-word
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias ssh="kitten ssh"
