01796bf2130aed66b782f6f8c42d13dc88ea7e37
Den Leschenko <den.leschenko@gmail.com>
2026-03-22 14:07:11 +0100

    sync settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,5 +6,4 @@ if status is-interactive
 end
 bind alt-backspace backward-kill-word
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-alias config-sync='config add ~/.local/share/krusader/ ~/.local/share/kxmlgui5/ ~/.local/share/kwin/ ~/.local/share/krdc/ ~/.config/fish/ ~/.config/kitty/ ~/.config/powershell/ ~/.config/teams-for-linux/Preferences ~/.config/yazi/ ~/.config/btop/ ~/.config/alacritty/ ~/.config/zed/ ~/.config/mc/ ~/.config/gtk-3.0/ ~/.config/gtk-4.0/'
 alias ssh="kitten ssh"
