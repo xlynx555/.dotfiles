002a9761d5d9803bd6268499e38e8361ccac8e09
Den Leschenko <den.leschenko@gmail.com>
2026-03-22 14:07:44 +0100

    sync settings

---
+++ .config/fish/functions/config-sync.fish
@@ -0,0 +1,5 @@
+function config-sync
+    config add ~/.local/share/krusader/ ~/.local/share/kxmlgui5/ ~/.local/share/kwin/ ~/.local/share/krdc/ ~/.config/fish/ ~/.config/kitty/ ~/.config/powershell/ ~/.config/teams-for-linux/Preferences ~/.config/yazi/ ~/.config/btop/ ~/.config/alacritty/ ~/.config/zed/ ~/.config/mc/ ~/.config/gtk-3.0/ ~/.config/gtk-4.0/
+    config commit -a -m "sync settings"
+    config push
+end
